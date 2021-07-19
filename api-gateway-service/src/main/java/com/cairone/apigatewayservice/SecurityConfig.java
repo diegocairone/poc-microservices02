8ce977cade0a2ace2f07a93c11f319e836a75a78
Diego Cairone <diegocairone@gmail.com>
2021-07-18 23:15:27 -0300

    Part #2
    Api gateway and security

---
+++ api-gateway-service/src/main/java/com/cairone/apigatewayservice/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.cairone.apigatewayservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults; 
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+            .oauth2Login(withDefaults());
+        http.csrf().disable();
+        return http.build();
+    }
+}
